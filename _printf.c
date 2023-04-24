9702d73adef322277447d6f5394a3d66b01d507d
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-23 19:46:42 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * _printf - formatted output conversion and print data
- * @format: input string
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return: the number of chars printed
+ * Return: number of chars printed.
  */
-
 int _printf(const char *format, ...)
 {
 unsigned int i = 0, len = 0, ibuf = 0;
@@ -13,7 +12,7 @@ va_list arguments;
 int (*function)(va_list, char *, unsigned int);
 char *buffer;
 
-va_start(arguments, format), buffer = malloc(sizeof(char) * BUFFER_SIZE);
+va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
 if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 return (-1);
 if (!format[i])
@@ -43,9 +42,10 @@ i += ev_printf(format, i + 1);
 }
 else
 handl_buffer(buffer, format[i], ibuf), len++;
-for (ibuf = len; ibuf > BUFFER_SIZE; ibuf -= BUFFER_SIZE)
+for (ibuf = len; ibuf > 1024; ibuf -= 1024)
 ;
 }
 print_buffer(buffer, ibuf), free(buffer), va_end(arguments);
 return (len);
 }
+
